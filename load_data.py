367654c6b7b4117a70ee23a5c28a787eefadd0d2
Swapnil Singh <51404379+The-Swapster@users.noreply.github.com>
2021-09-01 09:45:15 +0530

    Create load_data.py

---
+++ load_data.py
@@ -0,0 +1,123 @@
+def load_data(dir_list, image_size):
+    """
+    Read images, resize and normalize them. 
+    Arguments:
+        dir_list: list of strings representing file directories.
+    Returns:
+        X: A numpy array with shape = (#_examples, image_width, image_height, #_channels)
+        y: A numpy array with shape = (#_examples, 1)
+    """
+
+    # load all images in a directory
+    X = []
+    y = []
+    image_width, image_height = image_size
+    
+    for directory in dir_list:
+        for filename in listdir(directory):
+            # load the image
+            image = cv2.imread(directory + '\\' + filename)
+            # crop the brain and ignore the unnecessary rest part of the image
+            image = crop_brain_contour(image, plot=False)
+            # resize image
+            image = cv2.resize(image, dsize=(image_width, image_height), interpolation=cv2.INTER_CUBIC)
+            # normalize values
+            image = image / 255.
+            # convert image to numpy array and append it to X
+            X.append(image)
+            # append a value of 1 to the target array if the image
+            # is in the folder named 'yes', otherwise append 0.
+            if directory[-3:] == 'yes':
+                y.append([1])
+            else:
+                y.append([0])
+                
+    X = np.array(X)
+    y = np.array(y)
+    
+    # Shuffle the data
+    X, y = shuffle(X, y)
+    print(f'Number of examples is: {len(X)}')
+    print(f'X shape is: {X.shape}')
+    print(f'y shape is: {y.shape}')
+    
+    return X, y
+
+augmented_path = 'augmented_dataset/'
+
+# augmented data (yes and no) contains both the original and the new generated examples
+augmented_yes = augmented_path + 'yes' 
+augmented_no = augmented_path + 'no'
+
+IMG_WIDTH, IMG_HEIGHT = (240, 240)
+
+X, y = load_data([augmented_yes, augmented_no], (IMG_WIDTH, IMG_HEIGHT))
+
+def plot_sample_images(X, y, n=50):
+    """
+    Plots n sample images for both values of y (labels).
+    Arguments:
+        X: A numpy array with shape = (#_examples, image_width, image_height, #_channels)
+        y: A numpy array with shape = (#_examples, 1)
+    """
+    
+    for label in [0,1]:
+        # grab the first n images with the corresponding y values equal to label
+        images = X[np.argwhere(y == label)]
+        n_images = images[:n]
+        
+        columns_n = 10
+        rows_n = int(n/ columns_n)
+
+        plt.figure(figsize=(20, 10))
+        
+        i = 1 # current plot        
+        for image in n_images:
+            plt.subplot(rows_n, columns_n, i)
+            plt.imshow(image[0])
+            
+            # remove ticks
+            plt.tick_params(axis='both', which='both', 
+                            top=False, bottom=False, left=False, right=False,
+                           labelbottom=False, labeltop=False, labelleft=False, labelright=False)
+            
+            i += 1
+        
+        label_to_str = lambda label: "Yes" if label == 1 else "No"
+        plt.suptitle(f"Brain Tumor: {label_to_str(label)}")
+        plt.show()
+        
+plot_sample_images(X, y)
+
+def split_data(X, y, test_size=0.2):
+       
+    """
+    Splits data into training, development and test sets.
+    Arguments:
+        X: A numpy array with shape = (#_examples, image_width, image_height, #_channels)
+        y: A numpy array with shape = (#_examples, 1)
+    Returns:
+        X_train: A numpy array with shape = (#_train_examples, image_width, image_height, #_channels)
+        y_train: A numpy array with shape = (#_train_examples, 1)
+        X_val: A numpy array with shape = (#_val_examples, image_width, image_height, #_channels)
+        y_val: A numpy array with shape = (#_val_examples, 1)
+        X_test: A numpy array with shape = (#_test_examples, image_width, image_height, #_channels)
+        y_test: A numpy array with shape = (#_test_examples, 1)
+    """
+    
+    X_train, X_test_val, y_train, y_test_val = train_test_split(X, y, test_size=test_size)
+    X_test, X_val, y_test, y_val = train_test_split(X_test_val, y_test_val, test_size=0.5)
+    
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+X_train, y_train, X_val, y_val, X_test, y_test = split_data(X, y, test_size=0.3)
+
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of development examples = " + str(X_val.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(y_train.shape))
+print ("X_val (dev) shape: " + str(X_val.shape))
+print ("Y_val (dev) shape: " + str(y_val.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(y_test.shape))
