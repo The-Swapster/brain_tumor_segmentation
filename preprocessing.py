177b893253094fa7c319921568e88916de6aba6f
Swapnil Singh <51404379+The-Swapster@users.noreply.github.com>
2021-09-01 09:43:26 +0530

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,151 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, Input, ZeroPadding2D, BatchNormalization, Activation, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score
+from sklearn.utils import shuffle
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import cv2
+import imutils
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from os import listdir
+
+%matplotlib inline
+
+def crop_brain_contour(image, plot=False):
+    
+    #import imutils
+    #import cv2
+    #from matplotlib import pyplot as plt
+    
+    # Convert the image to grayscale, and blur it slightly
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    # Threshold the image, then perform a series of erosions +
+    # dilations to remove any small regions of noise
+    thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.erode(thresh, None, iterations=2)
+    thresh = cv2.dilate(thresh, None, iterations=2)
+
+    # Find contours in thresholded image, then grab the largest one
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    c = max(cnts, key=cv2.contourArea)
+    
+
+    # Find the extreme points
+    extLeft = tuple(c[c[:, :, 0].argmin()][0])
+    extRight = tuple(c[c[:, :, 0].argmax()][0])
+    extTop = tuple(c[c[:, :, 1].argmin()][0])
+    extBot = tuple(c[c[:, :, 1].argmax()][0])
+    
+    # crop new image out of the original image using the four extreme points (left, right, top, bottom)
+    new_image = image[extTop[1]:extBot[1], extLeft[0]:extRight[0]]            
+
+    if plot:
+        plt.figure()
+        plt.subplot(1, 2, 1)
+        plt.imshow(image)
+        
+        plt.tick_params(axis='both', which='both', 
+                        top=False, bottom=False, left=False, right=False,
+                        labelbottom=False, labeltop=False, labelleft=False, labelright=False)
+        
+        plt.title('Original Image')
+            
+        plt.subplot(1, 2, 2)
+        plt.imshow(new_image)
+
+        plt.tick_params(axis='both', which='both', 
+                        top=False, bottom=False, left=False, right=False,
+                        labelbottom=False, labeltop=False, labelleft=False, labelright=False)
+
+        plt.title('Cropped Image')
+        
+        plt.show()
+    
+    return new_image
+    
+ex_img = cv2.imread('brain_tumor_dataset/yes/Y1.jpg')
+ex_new_img = crop_brain_contour(ex_img, True)#Crop image according to extreme points   
+
+# Nicely formatted time string
+def hms_string(sec_elapsed):
+    h = int(sec_elapsed / (60 * 60))
+    m = int((sec_elapsed % (60 * 60)) / 60)
+    s = sec_elapsed % 60
+    return f"{h}:{m}:{round(s,1)}"
+
+def augment_data(file_dir, n_generated_samples, save_to_dir):
+    """
+    Arguments:
+        file_dir: A string representing the directory where images that we want to augment are found.
+        n_generated_samples: A string representing the number of generated samples using the given image.
+        save_to_dir: A string representing the directory in which the generated images will be saved.
+    """
+    
+    #from keras.preprocessing.image import ImageDataGenerator
+    #from os import listdir
+    
+    data_gen = ImageDataGenerator(rotation_range=10, 
+                                  width_shift_range=0.1, 
+                                  height_shift_range=0.1, 
+                                  shear_range=0.1, 
+                                  brightness_range=(0.3, 1.0),
+                                  horizontal_flip=True, 
+                                  vertical_flip=True, 
+                                  fill_mode='nearest'
+                                 )
+
+    
+    for filename in listdir(file_dir):
+        # load the image
+        image = cv2.imread(file_dir + '\\' + filename)
+        # reshape the image
+        image = image.reshape((1,)+image.shape)
+        # prefix of the names for the generated sampels.
+        save_prefix = 'aug_' + filename[:-4]
+        # generate 'n_generated_samples' sample images
+        i=0
+        for batch in data_gen.flow(x=image, batch_size=1, save_to_dir=save_to_dir, 
+                                           save_prefix=save_prefix, save_format='jpg'):
+            i += 1
+            if i > n_generated_samples:
+                break
+                
+start_time = time.time()
+augmented_data_path = 'augmented_dataset/'
+
+# augment data for the examples with label equal to 'yes' representing tumurous examples
+augment_data(file_dir='brain_tumor_dataset/yes', n_generated_samples=6, save_to_dir=augmented_data_path+'yes')
+# augment data for the examples with label equal to 'no' representing non-tumurous examples
+augment_data(file_dir='brain_tumor_dataset/no', n_generated_samples=9, save_to_dir=augmented_data_path+'no')
+
+end_time = time.time()
+execution_time = (end_time - start_time)
+print(f"Elapsed time: {hms_string(execution_time)}")
+
+def data_summary(main_path):
+    
+    yes_path = main_path+'yes'
+    no_path = main_path+'no'
+        
+    # number of files (images) that are in the the folder named 'yes' that represent tumorous (positive) examples
+    m_pos = len(listdir(yes_path))
+    # number of files (images) that are in the the folder named 'no' that represent non-tumorous (negative) examples
+    m_neg = len(listdir(no_path))
+    # number of all examples
+    m = (m_pos+m_neg)
+    
+    pos_prec = (m_pos* 100.0)/ m
+    neg_prec = (m_neg* 100.0)/ m
+    
+    print(f"Number of examples: {m}")
+    print(f"Percentage of positive examples: {pos_prec}%, number of pos examples: {m_pos}") 
+    print(f"Percentage of negative examples: {neg_prec}%, number of neg examples: {m_neg}")
+
+data_summary(augmented_data_path)
